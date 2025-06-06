2e4fbb444baff16f4c5d80462f3997eda7b217d0
Riccardo Spagni <ric@spagni.net>
2025-06-06 10:35:20 +0200

    Format code with go fmt

--- cmd/mcp-compose/main.go
+++ cmd/mcp-compose/main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 
-    "mcpcompose/internal/cmd"
+	"mcpcompose/internal/cmd"
 )
 
 var version = "dev"
 
 func main() {
-    rootCmd := cmd.NewRootCommand(version)
-    if err := rootCmd.Execute(); err != nil {
-        fmt.Fprintf(os.Stderr, "Error: %s\n", err)
-        os.Exit(1)
-    }
+	rootCmd := cmd.NewRootCommand(version)
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		os.Exit(1)
+	}
 }
